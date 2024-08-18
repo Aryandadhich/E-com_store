b98b9a62afc510c9e5e500b9c09da30d662adcb3
adadheec <Aryan.Dadheech@capgemini.com>
2024-08-18 18:10:26 +0530

    Client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App/layout/App.tsx'
+import './App/layout/style.css'
 //strictmode is actuyally makes 2 request when we are going to get a resource
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
