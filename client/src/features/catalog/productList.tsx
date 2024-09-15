1e16e2bea013fb832438e1def73939f6068536f3
adadheec <Aryan.Dadheech@capgemini.com>
2024-09-15 23:54:54 +0530

    list

--- client/src/features/catalog/productList.tsx
+++ client/src/features/catalog/productList.tsx
@@ -11,8 +11,8 @@ export default function productList({products}: Props){
       return (
         <Grid container spacing={4}>
         {products.map(product => (
-            <Grid item xs={3}>
-             <ProductCard key={product.id} product={product} darkMode={false}/>
+            <Grid item xs={3} key={product.id}>
+             <ProductCard product={product} darkMode={false}/>
             </Grid>
             
         ))}
