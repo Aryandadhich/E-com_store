b792f95255da18c29b7069d8474db0e87ade12d5
adadheec <Aryan.Dadheech@capgemini.com>
2024-09-17 23:26:03 +0530

    header

--- client/src/App/layout/App.tsx
+++ client/src/App/layout/App.tsx
@@ -16,7 +16,7 @@ function App() {
       }
     }
    })
-
+ 
    function handleThemeChange() {
          setDarkMode(!darkMode); 
    }
--- client/src/App/layout/Header.tsx
+++ client/src/App/layout/Header.tsx
@@ -1,4 +1,16 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { List,AppBar, ListItem, Switch, Toolbar, Typography, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  {title: 'catalog', path: '/catalog'},
+  {title: 'about', path: '/about'},
+  {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+  {title: 'login', path: '/login'},
+  {title: 'register', path: '/register'},
+]
 
 interface Props{
   darkmode: boolean;
@@ -13,6 +25,17 @@ export default function Header({darkmode, handleThemeChange} : Props){
               RE-STORE
             </Typography>
             <Switch  checked={darkmode} onChange={handleThemeChange}/>
+            <List sx={{display: 'flex', alignItems:'center'}}>
+                {midLinks.map(({title, path}) =>(
+                  <Button
+                        component={NavLink}
+                        to={path}
+                        key={path}
+                        sx={{color: 'inherit',typography: 'h6',textDecoration: 'none','&:hover':{textDecoration: 'underline'}}}>
+                 {title.toUpperCase()}
+               </Button>
+               ))}
+            </List>
            </Toolbar>
          </AppBar>
     )
