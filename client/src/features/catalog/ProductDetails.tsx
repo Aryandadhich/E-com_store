9ebdc9a6839caee1a05da2ed21048d907249be3a
adadheec <Aryan.Dadheech@capgemini.com>
2024-09-18 14:48:36 +0530

    Axios

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,17 +1,28 @@
 import { Typography } from "@mui/material";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Product } from "../../App/Models/product";
 
 export default function ProductDetails(){
-    useParams<{ id: string; }>();
+    const {id} = useParams<{ id: string; }>();
+    const [product, setproduct] = useState<Product | null>(null); 
+    const [loading, setloading] = useState(true);
 
     useEffect(() =>{
-        axios.get(`http://localhost:5000/api/products`)
-    })
+        axios.get(`http://localhost:5000/api/products/${id}`)
+        .then(response => setproduct(response.data))
+        .catch(error => console.log(error))
+        .finally(() => setloading(false));
+    },[id])
+     
+    if(loading) return <h3>Loading...</h3>
+
+    if(!product) return <h3>Product not found</h3>
+
     return(
     <Typography variant="h2">
-        Product Detail
+       {product.name}
     </Typography>
     )
 }
